bfcae825053d3ec51306cc5c0105ad56864e5156
Viktor Modroczký <v.modroczky@gmail.com>
2023-11-25 22:56:28 +0100

    Use numpydoc

--- src/aspectpy/decorators.py
+++ src/aspectpy/decorators.py
@@ -10,16 +10,24 @@ def validate_after_returning_action(func: Callable[..., Any]):
     Decorator that checks if the action to be used in `AfterReturning`
     has the correct signature, i.e. has a parameter named `_RETURNED_VAL_`.
 
-    Parameters:
-        - `func`: The action to be used in AfterReturning.
+    Parameters
+    ----------
+    func : Callable
+        The action to be used in `AfterReturning`.
 
-    Returns:
-        - wrapper function.
+    Returns
+    -------
+    Callable
+        Wrapper function.
 
-    Raises:
-        - `ValueError`: If the action does not have a parameter named `_RETURNED_VAL_`.
-        - `ValueError`: If the action is already decorated with this decorator.
+    Raises
+    ------
+    ValueError
+        If the action does not have a parameter named `_RETURNED_VAL_`.
+    ValueError
+        If the action is already decorated with this decorator.
     """
+
     if hasattr(func, FLAG_VALIDATED) and getattr(func, FLAG_VALIDATED):
         raise ValueError(
             f"{func.__qualname__} is already decorated with @{validate_after_returning_action.__name__}"
@@ -53,29 +61,39 @@ def mutate_params(
     func_signature: Signature,
 ) -> tuple[tuple[Any, ...], dict[str, Any]]:
     """
-    Mutates the `args` and `kwargs` with the new parameters. If the update is
-    executed, the `args` tuple gets emptied and the `kwargs` dictionary gets
-    filled with all the updated, new or original parameters if some are missing
-    from the `new_params` dictionary. Effectively, this decision does not affect
-    code functionality, but rather is just a design choice. It allows for the
-    `args` and `kwargs` to be updated by name and the result is that every parameter
+    Mutates arguments and keyword arguments with new parameters. If the update is
+    executed, the `args` tuple gets emptied, and the `kwargs` dictionary gets filled
+    with all the updated, new, or original parameters if some are missing from the
+    `new_params` dictionary. Effectively, this decision does not affect code
+    functionality, but rather is just a design choice. It allows for the arguments
+    and keyword arguments to be updated by name, and the result is that every parameter
     becomes a keyword argument.
 
-    Parameters:
-        - `args`: The original args.
-        - `kwargs`: The original kwargs.
-        - `new_params`: The new params dictionary with key to value mappings.
+    Parameters
+    ----------
+    args : tuple
+        The original arguments.
 
-            Can include args and kwargs as `new_params[arg_name] = value` and
-            `new_params[kwarg_name] = value` respectively.
+    kwargs : dict
+        The original keyword arguments.
 
-            If `None` or empty, the original parameters are used.
+    new_params : dict or None
+        Dictionary with key to value mappings representing new parameters.
+        If `None` or empty, the original parameters are used.
 
-        - `func_signature`: The signature of the function.
+        Can include both arguments and keyword arguments as
+        `new_params[arg_name] = value` and `new_params[kwarg_name] = value`
+        respectively.
 
-    Returns:
-        - `tuple` of args and kwargs after mutation.
+    func_signature : Signature
+        The signature of the function.
+
+    Returns
+    -------
+    tuple
+        Tuple of arguments and keyword arguments after mutation.
     """
+
     if new_params is None:
         return args, kwargs
 
@@ -93,20 +111,29 @@ class Before:
     """
     Decorator that executes an action before the decorated function is called.
 
-    Parameters:
-        - `params_update`: The new params dictionary with key to value mappings.
+    Parameters
+    ----------
+    params_update : dict or None
+        Dictionary with key to value mappings representing new parameters.
+        If `None` or empty, the original parameters are used.
 
-            Can include args and kwargs as `params_update[arg_name] = value` and
-            `params_update[kwarg_name] = value` respectively.
+        Can include both arguments and keyword arguments as
+        `new_params[arg_name] = value` and `new_params[kwarg_name] = value`
+        respectively.
 
-            If `None` or empty, the original parameters are used.
+    action : Callable
+        The action to be executed before the decorated function is called.
 
-        - `action`: The action to be executed before the decorated function is called.
-        - `action_args`: The args to be passed to the action.
-        - `action_kwargs`: The kwargs to be passed to the action.
+    action_args : tuple
+        The arguments to be passed to the action.
 
-    Returns:
-        - wrapper function after instance of this class is called.
+    action_kwargs : dict
+        The keyword arguments to be passed to the action.
+
+    Returns
+    -------
+    Callable
+        Wrapper function after instance of this class is called.
     """
 
     def __init__(
@@ -137,24 +164,31 @@ class AfterReturning:
     """
     Decorator that executes an action after the decorated function is called and returns.
 
-    Parameters:
-        - `params_update`: The new params dictionary with key to value mappings.
+    Parameters
+    ----------
+    params_update : dict or None
+        Dictionary with key to value mappings representing new parameters.
+        If `None` or empty, the original parameters are used.
 
-            Can include args and kwargs as `params_update[arg_name] = value` and
-            `params_update[kwarg_name] = value` respectively.
+        Can include both arguments and keyword arguments as
+        `new_params[arg_name] = value` and `new_params[kwarg_name] = value`
+        respectively.
 
-            If `None` or empty, the original parameters are used.
+    action : Callable
+        The action to be executed after the decorated function is called and returns.
+        This action must have a parameter named `_RETURNED_VAL_` and must be decorated with
+        `@validate_after_returning_action`.
 
-        - `action`: The action to be executed after the decorated function is called and returns.
+    action_args : tuple
+        The arguments to be passed to the action.
 
-            This action must have a parameter named `_RETURNED_VAL_` and must be decorated with
-            `@validate_after_returning_action`.
+    action_kwargs : dict
+        The keyword arguments to be passed to the action.
 
-        - `action_args`: The args to be passed to the action.
-        - `action_kwargs`: The kwargs to be passed to the action.
-
-    Returns:
-        - wrapper function after instance of this class is called.
+    Returns
+    -------
+    Callable
+        Wrapper function after instance of this class is called.
     """
 
     def __init__(
@@ -190,24 +224,32 @@ class AfterThrowing:
     """
     Decorator that executes an action after the decorated function is called and throws an exception.
 
-    Parameters:
-        - `params_update`: The new params dictionary with key to value mappings.
+    Parameters
+    ----------
+    params_update : dict or None
+        Dictionary with key to value mappings representing new parameters.
+        If `None` or empty, the original parameters are used.
 
-            Can include args and kwargs as `params_update[arg_name] = value` and
-            `params_update[kwarg_name] = value` respectively.
+        Can include both arguments and keyword arguments as
+        `new_params[arg_name] = value` and `new_params[kwarg_name] = value`
+        respectively.
 
-            If `None` or empty, the original parameters are used.
+    exceptions : Exception or tuple of Exceptions or None
+        The exceptions that trigger the action. If `None`, all exceptions trigger the action.
 
-        - `exceptions`: The exceptions that trigger the action.
+    action : Callable
+        The action to be executed after the decorated function is called and throws an exception.
 
-            If `None`, all exceptions trigger the action.
+    action_args : tuple
+        The arguments to be passed to the action.
 
-        - `action`: The action to be executed after the decorated function is called and throws an exception.
-        - `action_args`: The args to be passed to the action.
-        - `action_kwargs`: The kwargs to be passed to the action.
+    action_kwargs : dict
+        The keyword arguments to be passed to the action.
 
-    Returns:
-        - wrapper function after instance of this class is called.
+    Returns
+    -------
+    Callable
+        Wrapper function after instance of this class is called.
     """
 
     def __init__(
@@ -243,21 +285,32 @@ class Around:
     Decorator that executes an action instead of the decorated function if the proceed condition is met.
     Else, the decorated function is called.
 
-    Parameters:
-        - `params_update`: The new params dictionary with key to value mappings.
+    Parameters
+    ----------
+    params_update : dict or None
+        Dictionary with key to value mappings representing new parameters.
+        If `None` or empty, the original parameters are used.
 
-            Can include args and kwargs as `params_update[arg_name] = value` and
-            `params_update[kwarg_name] = value` respectively.
+        Can include both arguments and keyword arguments as
+        `new_params[arg_name] = value` and `new_params[kwarg_name] = value`
+        respectively.
 
-            If `None` or empty, the original parameters are used.
+    proceed : bool or callable
+        The proceed condition. Can be a boolean or a callable that returns a boolean.
 
-        - `proceed`: The proceed condition. Can be a boolean or a callable that returns a boolean.
-        - `action`: The action to be executed instead of the decorated function if the proceed condition is met.
-        - `action_args`: The args to be passed to the action.
-        - `action_kwargs`: The kwargs to be passed to the action.
+    action : Callable
+        The action to be executed instead of the decorated function if the proceed condition is met.
 
-    Returns:
-        - wrapper function after instance of this class is called.
+    action_args : tuple
+        The arguments to be passed to the action.
+
+    action_kwargs : dict
+        The keyword arguments to be passed to the action.
+
+    Returns
+    -------
+    Callable
+        Wrapper function after instance of this class is called.
     """
 
     def __init__(
